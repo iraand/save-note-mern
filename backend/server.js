b335633a1d32b8bd973d59e8fca5ef40510173be
iraand <iraand@bigmir.net>
2022-10-08 01:06:26 +0300

    REST API

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,11 @@
 const express = require('express');
+const colors = require('colors');
 const dotenv = require('dotenv').config();
-const {errorHandler} = require('./middleware/error.middleware')
+const {errorHandler} = require('./middleware/error.middleware');
+const connectDB = require('./config/db');
 const port = process.env.PORT || 5000;
+
+connectDB();
 
 const app = express();
 
@@ -9,5 +13,7 @@ app.use(express.json());
 app.use(express.urlencoded({extended: false}))
 app.use('/api/goals', require('./routs/routs'))
 app.use(errorHandler)
-app.listen(port, () => console.log(`Server started on port ${port}`))
+app.listen(port, () =>
+ console.log(`Server started on port ${port}`.bgBrightMagenta)
+);
 
