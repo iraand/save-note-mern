938e9a9355763443adfcab2f4030921c5880daa9
iraand <iraand@bigmir.net>
2022-10-25 13:15:19 +0300

    made build

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const path = require('path')
 const colors = require('colors');
 const dotenv = require('dotenv').config();
 const {errorHandler} = require('./middleware/error.middleware');
@@ -13,6 +14,14 @@ app.use(express.json());
 app.use(express.urlencoded({extended: false}))
 app.use('/api/notes', require('./routs/note.routs'))
 app.use("/api/users", require("./routs/user.routs"));
+
+// Serve frontend
+if(process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '../frontend/build')))
+
+    app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, '../', 'frontend', 'build', 'index.html')))
+}
+
 app.use(errorHandler)
 app.listen(port, () =>
  console.log(`Server started on port ${port}`.bgBrightMagenta)
